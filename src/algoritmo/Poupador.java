4818073a2913562725d1046225ab37ccbd027398
felipe2121 <felipe.holanda12@yahoo.com.br>
2022-03-16 18:03:49 -0300

    x

--- src/algoritmo/Poupador.java
+++ src/algoritmo/Poupador.java
@@ -1,6 +1,5 @@
 package algoritmo;//package algoritmo;
 
-import algoritmo.path.Graph;
 import controle.Constantes;
 import java.awt.*;
 import java.util.*;
@@ -187,7 +186,7 @@ public class Poupador extends ProgramaPoupador {
 		if(y != -1){
 			return y;
 		}
-		if (graph.canRouteToBank(pointNow.x, pointNow.y) && sensor.getNumeroDeMoedas() > 3 && fear == -1) {
+		if (graph.canRouteToBank(pointNow.x, pointNow.y) && sensor.getNumeroDeMoedas() > 3 && fear > 1000) {
 			System.out.println("ENTROU");
 			pointsToGo.addAll(graph.pointsToGo(pointNow));
 
@@ -219,7 +218,6 @@ public class Poupador extends ProgramaPoupador {
 	}
 
 	private int explore(){
-		System.out.println("explore");
 		Point pointNow = sensor.getPosicao();
 		int[] see = sensor.getVisaoIdentificacao();
 		int bancox = 8 - pointNow.x;
@@ -272,7 +270,7 @@ public class Poupador extends ProgramaPoupador {
 				soma += chances.get(i);
 				if(ran < soma){
 					for(int j = 1; j < 5; j++){
-						if(positionsChance.get(j) == chances.get(i)){
+						if(Objects.equals(positionsChance.get(j), chances.get(i))){
 							return j;
 						}
 					}
@@ -280,6 +278,167 @@ public class Poupador extends ProgramaPoupador {
 			}
 		}
 
-		return 0;
+		return (int) (Math.random() * 5);
+	}
+}
+
+class Graph {
+	private final int vertices_size;
+	private final ArrayList<ArrayList<Integer>> adj;
+
+	public Graph(int vertices_size) {
+		this.vertices_size = vertices_size;
+
+		// Adjacency list for storing which vertices are connected
+		adj = new ArrayList<ArrayList<Integer>>(vertices_size);
+
+		for (int i = 0; i < vertices_size; i++) {
+			adj.add(new ArrayList<Integer>());
+		}
+	}
+
+	public int mapPositions(int pos_x, int pos_y) {
+		// pos_x = 29, pos_y = 29 -> 900
+		// pos_x =  1, pos_y =  0 ->  30
+		int count = 0;
+		if(pos_y != 0){
+			pos_y -=1;
+		}
+		for (int y = 0; y < pos_y*30; y++) {
+			count++;
+		}
+
+		if (pos_x == 0) {
+			return count;
+		} else {
+			for (int x = 0; x < pos_x; x++) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int[][] mapPositionsToPosition(int num_v) {
+		// 900 -> pos_x = 29, pos_y = 29
+		// 30 -> pos_x = 0, pos_y = 1
+		int pos_x = ((num_v+1)%30) - 1;
+		int pos_y = Math.abs((num_v+1)/30);
+
+		if ((num_v+1)%30 == 0) {
+			if(pos_y == 0) {
+				pos_x = 0;
+			} else {
+				pos_x = 29;
+			}
+		}
+
+		return new int[][]{{pos_x}, {pos_y}};
+	}
+
+	public void addEdge2(int pos_x, int pos_y, int pos_x2, int pos_y2) {
+		int current_v = mapPositions(pos_x, pos_y);
+		int linked_v = mapPositions(pos_x2, pos_y2);
+
+		adj.get(current_v).add(linked_v);
+		adj.get(linked_v).add(current_v);
+	}
+
+	public Boolean canRouteToBank(int pos_x, int pos_y) {
+		int source_v = mapPositions(pos_x, pos_y);
+		int destination_v = mapPositions(8, 8);
+
+		int[] predecessor = new int[vertices_size];
+		int[] distance = new int[vertices_size];
+
+		return BFS(source_v, destination_v, vertices_size, predecessor, distance);
+	}
+
+	public LinkedList<Point> pointsToGo(Point pointNow) {
+		LinkedList<Integer> shortestPathToBank = shortestDistance(pointNow.x, pointNow.y, 8 ,8);
+
+		LinkedList<Point> pointsToGoList = new LinkedList<>();
+		for (int i = 2; i <= shortestPathToBank.size()-2; i=i+2) {
+			Point nextPoint = new Point(shortestPathToBank.get(i), shortestPathToBank.get(i+1));
+			pointsToGoList.add(nextPoint);
+		}
+		return pointsToGoList;
+	}
+
+	public LinkedList<Integer> shortestDistance(int pos_x, int pos_y, int pos_x2, int pos_y2) {
+		int source_v = mapPositions(pos_x, pos_y);
+		int destination_v = mapPositions(pos_x2, pos_y2);
+
+		int[] predecessor = new int[vertices_size];
+		int[] distance = new int[vertices_size];
+
+		if (!BFS(source_v, destination_v, vertices_size, predecessor, distance)) {
+
+		}
+
+		// LinkedList to store path
+		LinkedList<Integer> path = new LinkedList<Integer>();
+		int crawl = destination_v;
+		path.add(crawl);
+		while (predecessor[crawl] != -1) {
+			path.add(predecessor[crawl]);
+			crawl = predecessor[crawl];
+		}
+
+		LinkedList<Integer> result = new LinkedList<>();
+		for (int i = path.size() - 1; i >= 0; i--) {
+			result.add(mapPositionsToPosition(path.get(i))[0][0]);
+			result.add(mapPositionsToPosition(path.get(i))[1][0]);
+		}
+
+		return result;
+	}
+
+	// a modified version of BFS that stores predecessor
+	// of each vertex in array pred
+	// and its distance from source in array dist
+	boolean BFS(int src, int dest, int v, int[] pred, int[] dist) {
+		// a queue to maintain queue of vertices whose
+		// adjacency list is to be scanned as per normal
+		// BFS algorithm using LinkedList of Integer type
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+
+		// boolean array visited[] which stores the
+		// information whether ith vertex is reached
+		// at least once in the Breadth first search
+		boolean[] visited = new boolean[v];
+
+		// initially all vertices are unvisited
+		// so v[i] for all i is false
+		// and as no path is yet constructed
+		// dist[i] for all i set to infinity
+		for (int i = 0; i < v; i++) {
+			visited[i] = false;
+			dist[i] = Integer.MAX_VALUE;
+			pred[i] = -1;
+		}
+
+		// now source is first to be visited and
+		// distance from source to itself should be 0
+		visited[src] = true;
+		dist[src] = 0;
+		queue.add(src);
+
+		// bfs Algorithm
+		while (!queue.isEmpty()) {
+			int u = queue.remove();
+			for (int i = 0; i < adj.get(u).size(); i++) {
+				if (!visited[adj.get(u).get(i)]) {
+					visited[adj.get(u).get(i)] = true;
+					dist[adj.get(u).get(i)] = dist[u] + 1;
+					pred[adj.get(u).get(i)] = u;
+					queue.add(adj.get(u).get(i));
+
+					// stopping condition (when we find
+					// our destination)
+					if (adj.get(u).get(i) == dest) return true;
+				}
+			}
+		}
+		return false;
 	}
 }
